c8c8a1dc4c600d412024136734c03f5cb5cc5b8e
Ashish Munot <ashish.munot22@gmail.com>
2023-08-20 13:20:53 +0530

    hero-qtify-takehome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from "./components/NavBar/NavBar";
-import "./styles.css";
+import "./App.css";
+import Hero from "./components/Hero/Hero";
 
 export default function App() {
   return (
     <>
       <NavBar />
+      <Hero />
     </>
   );
 }
